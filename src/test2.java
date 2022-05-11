99f476fc9b78c57286e079e20a8d3b00193339de
Chen <chen2468830035@gmail.com>
2022-05-11 17:24:47 +0200

    update

--- src/test2.java
+++ src/test2.java
@@ -1,45 +1,29 @@
 public class test2
 {
-  public String Sum(int n)
+
+  // TODO: 11/05/2022 recursive method
+  public String recursive(int n)
   {
-    if (n <= 1){
-      return "1:"+n;
-    }
     if (n < 7){
       return "1:"+n;
     }
-    if (n == 7){
-      return "7:1";
-    }
-    if (n == 10){
-      return "10:1";
-    }
-    if (n == 20){
-      return "20:1";
-    }
-    if (n == 22){
-      return "22:1";
-    }
-
-    // TODO: 11/05/2022 recursive method
+    
     if (n < 10){
       int e = n/7;
       n = n%7;
-      return Sum(n)+" 7:"+e;
+      return recursive(n)+" 7:"+e;
     }
-
-    // TODO: 11/05/2022 recursive method
+    
     if (n < 22){
       int e = n/10;
       n = n%10;
-      return Sum(n)+" 10:"+e;
+      return recursive(n)+" 10:"+e;
     }
-
-    // TODO: 11/05/2022 recursive method
+    
     if (n > 22){
       int e = n/22;
       n = n%22;
-      return Sum(n)+" 22:"+e;
+      return recursive(n)+" 22:"+e;
     }
 
     return "error";
@@ -50,6 +34,6 @@ public class test2
   {
     test2 t2 = new test2();
 
-    System.out.println(t2.Sum(146));
+    System.out.println(t2.recursive(146));
   }
 }
